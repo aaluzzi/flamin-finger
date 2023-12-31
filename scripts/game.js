f82c7bf73a883a833d9f86307dfd7570f9f24e43
Austin Aluzzi <austinaluzzi@gmail.com>
2023-12-31 14:28:18 -0800

    Add game score and round switching

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,32 @@
 import { animatePath, animateGridDraw, drawTimer, animateGridClear } from "./modules/graphics.js";
 import { PATH_TAKEN, generatePath } from "./modules/grid.js";
 
-const {grid, path} = generatePath();
-let pathIndex = 1;
+let game;
+let pathIndex;
 let status = "starting";
 
+let score;
 let gameTimerId;
 let gameStartTime;
-let timerLengthSeconds = path.length / 10;
+let timerLengthSeconds;
 
 function gameLoop(timestamp) {
     if (status === "starting") {
-        if (!animateGridDraw(timestamp, grid)) {
-            status = "started";
+        if (!animateGridDraw(timestamp, game.grid)) {
+            status = "running";
             gameStartTime = Date.now();
-            gameTimerId = setTimeout(lose, timerLengthSeconds * 1000);
+            gameTimerId = setTimeout(loseGame, timerLengthSeconds * 1000);
         }
-    } else if (status === "started") {
-        animatePath(timestamp, path, pathIndex);
+    } else if (status === "running") {
+        animatePath(timestamp, game.path, pathIndex);
         drawTimer(timerLengthSeconds - ((Date.now() - gameStartTime) / 1000));
-    } else if (status === "ending") {
+    } else if (status === "losing") {
         if (!animateGridClear(timestamp)) {
-            status = "ended";
+            status = "menu";
+        }
+    } else if (status === "switching") {
+        if (!animateGridClear(timestamp)) {
+            startRound();
         }
     }
 
@@ -29,37 +34,45 @@ function gameLoop(timestamp) {
 }
 window.requestAnimationFrame(gameLoop);
 
-function start() {
+startGame();
+
+function startGame() {
+    score = 0;
+    startRound();
+}
+
+function startRound() {
+    game = generatePath();
+    pathIndex = 1;
+    timerLengthSeconds = game.path.length / (10 + score);
     status = "starting";
 }
 
-function win() {
-    status = "ending";
-    console.log("win");
+function winRound() {
+    status = "switching";
+    score++;
     clearTimeout(gameTimerId);
 }
 
-function lose() {
-    status = "ending";
-    console.log("lost");
+function loseGame() {
+    status = "losing";
+    console.log(`Your score: ${score}`);
 }
 
 function traversePath() {
-    grid[path[pathIndex].x][path[pathIndex].y] = PATH_TAKEN;
+    game.grid[game.path[pathIndex].x][game.path[pathIndex].y] = PATH_TAKEN;
     
-    if (pathIndex === 0) {
-        start();
-    } else if (pathIndex === path.length - 1) {
-        win();
+    if (pathIndex === game.path.length - 1) {
+        winRound();
         return;
     }
     pathIndex++;
 }
 
 document.querySelector('canvas').addEventListener('mousemove', e => {
-    let gridX = e.offsetX / e.target.width * grid.length;
-    let gridY = e.offsetY / e.target.height * grid.length;
-    if (Math.abs(path[pathIndex].x - gridX) < 0.9 && Math.abs(path[pathIndex].y - gridY) < 0.9 && status !== "ended") {
+    let gridX = e.offsetX / e.target.width * game.grid.length;
+    let gridY = e.offsetY / e.target.height * game.grid.length;
+    if (Math.abs(game.path[pathIndex].x - gridX) < 0.9 && Math.abs(game.path[pathIndex].y - gridY) < 0.9 && status === "running") {
        traversePath();
     }
 })
